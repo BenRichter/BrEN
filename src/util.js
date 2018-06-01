faec83cea8a9a7629612826626ddc85c5e5c90d7
Ben Richter <bri@move-elevator.de>
2018-06-01 16:55:23 +0200

    Add serialization to encode and fixLength strings

--- src/util.js
+++ src/util.js
@@ -1,6 +1,32 @@
 /**
+ * Make all strings same length.
+ * > Brain.js needs comparable same length data.
+ *
+ * @param data
+ * @return {*}
+ */
+const fixLengths = (data) => {
+
+  let maxLengthInput = -1;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].input.length > maxLengthInput) {
+      maxLengthInput = data[i].input.length;
+    }
+  }
+
+  for (let i = 0; i < data.length; i++) {
+    while (data[i].input.length < maxLengthInput) {
+      data[i].input.push(0);
+    }
+  }
+
+  return data;
+}
+
+
+/**
  * Convert String to Array of float numbers.
- * Brain is better with numeric values. Always encode chars to numbers.
+ * > Brain.js is better with numeric values. Always encode chars to numbers.
  *
  * @param d
  * @return { Array[] }
@@ -12,6 +38,25 @@ const encode = d => {
   })
   return encoded
 }
-const encodeData = data => data.map( d => encode(d) )
 
-module.exports = encodeData()
+const encodeData = data => data.map( d => {
+  return {
+    input:  encode(d.input),
+    output: d.output
+  }
+})
+
+
+/**
+ * Normalize out data for usage
+ *
+ * @param data
+ * @return {*}
+ */
+const serialize = data => fixLengths(encodeData(data))
+
+module.exports = {
+  fixLengths:  fixLengths,
+  encode:     encode,
+  serialize:  serialize,
+}
