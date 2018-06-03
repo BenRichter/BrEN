a72cb6024f0d62610afdf05e653ad6d16576ee97
Ben Richter <ben.k.richter@gmail.com>
2018-06-03 17:17:41 +0200

    Add toFunction example

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const brain = require('brain.js')
 const trainingData  = require('./src/training-data')
 const util = require('./src/util')
 
+/**
+ * 0. Create a neural network
+ */
 const net = new brain.NeuralNetwork()
+
 
 /**
  * 1. Train our neural network on training-data
@@ -13,12 +17,21 @@ net.train(
   util.serialize(trainingData), {log: true}
 )
 
+
 /**
  * 2. Let's categorize some real data
  */
 const output = net.run(
   util.encode('Nothing is not ok')
 )
+
+/**
+ * 2b. Use net as a normal function
+ */
+// const detectMood = net.toFunction()
+// const output = detectMood(
+//   util.encode('Nothing is not ok')
+// )
 
 
 console.log(output)
