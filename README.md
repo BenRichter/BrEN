c6b9c80229d688b5ed95389cd48acf58ee5f3fe2
Ben Richter <ben.k.richter@gmail.com>
2018-06-03 18:14:59 +0200

    Add 3. Save and Export Net

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 https://github.com/BrainJS/brain.js 
 
 Brain is better with numeric values. Always encode chars to numbers.
+
+
+### Examples
+- Writing a children's book using a recurrent neural network
+- Simple letter detection
+
+https://github.com/BrainJS/brain.js/tree/develop/examples
+
+
+simple letter detection
--- index.js
+++ index.js
@@ -12,10 +12,17 @@ const net = new brain.NeuralNetwork()
 
 /**
  * 1. Train our neural network on training-data
+ * https://github.com/BrainJS/brain.js#training-options
  */
 net.train(
   fixLengths(encodeTrainingData(trainingData)),
-  {log: true}
+  {
+    // iterations: 20000,
+    // errorThresh: 0.005,
+    // learningRate: 0.3,
+    // timeout: 100000,
+    log: true
+  }
 )
 
 
@@ -36,4 +43,17 @@ const output = net.run(
 
 
 console.log(output)
+
+
+/**
+ * (3. Save and Export Net)
+ * a. via JSON
+ * b. as standalone function without brain.js
+ */
+// var json = detectMood.toJSON()
+// net.fromJSON(json)
+
+// console.log(detectMood.toString()) // copy and paste! no need to import brain.js
+
+
 
